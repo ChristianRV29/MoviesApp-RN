61bdb7727af3950e236565d1ddb47763fdc969b8
ChristianRV29 <kmylorv@gmail.com>
2022-01-05 10:18:52 -0500

    MovieDB typed response

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,11 +1,12 @@
 import React, { useEffect } from 'react';
 import { View, Text, Button } from 'react-native';
-import movieDB from '../api/movieDB';
+import movieDB from './../api/movieDB';
+import { MovieDBInterface } from './../interfaces/movie';
 
 export const HomeScreen: React.FC = ({ navigation }: any) => {
   useEffect(() => {
-    movieDB.get('/now_playing').then(data => {
-      console.log(data);
+    movieDB.get<MovieDBInterface>('/now_playing').then(resp => {
+      console.log(resp.data.results[0].id);
     });
   }, []);
 
