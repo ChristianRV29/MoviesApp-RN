d74f855a9d8db57aab967c4526ff7e6a1bb298c5
ChristianRV29 <kmylorv@gmail.com>
2022-01-05 10:33:08 -0500

    Custom hook useMovies created

---
+++ src/hooks/useMovies.tsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from 'react';
+
+import { MovieDBInterface, Movie } from './../interfaces/movie';
+import movieDB from './../api/movieDB';
+
+export const useMovies = () => {
+  const [movies, setMovies] = useState<Movie[]>([]);
+
+  const getMovies = async () => {
+    await movieDB
+      .get<MovieDBInterface>('/now_playing')
+      .then(resp => {
+        const { results } = resp.data;
+        if (results.length > 0) {
+          setMovies(results);
+        }
+      })
+      .catch(err =>
+        console.log('An error has been ocurred: ' + (err.message || err.error)),
+      );
+  };
+
+  useEffect(() => {
+    getMovies();
+  }, []);
+
+  return { movies };
+};
--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,14 +1,11 @@
-import React, { useEffect } from 'react';
+import React from 'react';
 import { View, Text, Button } from 'react-native';
-import movieDB from './../api/movieDB';
-import { MovieDBInterface } from './../interfaces/movie';
+import { useMovies } from '../hooks/useMovies';
 
 export const HomeScreen: React.FC = ({ navigation }: any) => {
-  useEffect(() => {
-    movieDB.get<MovieDBInterface>('/now_playing').then(resp => {
-      console.log(resp.data.results[0].id);
-    });
-  }, []);
+  const { movies } = useMovies();
+
+  console.log(movies[0]?.title);
 
   return (
     <View>
