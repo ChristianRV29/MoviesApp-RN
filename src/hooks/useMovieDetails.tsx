2ce49a75b4a35e648966d93441dd1e1e83ab8df5
ChristianRV29 <kmylorv@gmail.com>
2022-01-14 09:46:32 -0500

    MovieDetails througth movieId in API request

--- src/hooks/useMovieDetails.tsx
+++ src/hooks/useMovieDetails.tsx
@@ -1,15 +1,33 @@
-import React, { useState } from 'react';
+import { useState, useEffect } from 'react';
 
+import movieDB from './../api/movieDB';
+import { MovieDetailsResponse } from './../interfaces/movieDetails';
 interface MovieDetails {
   isLoading: boolean;
   movieDetails: any;
   cast: any[];
 }
 
-const useMovieDetails = () => {
+interface useMovieDetailsProps {
+  idMovie: number;
+}
+
+export const useMovieDetails = ({ idMovie }: useMovieDetailsProps) => {
   const [movieDetails, setmovieDetails] = useState<MovieDetails>();
 
-  return <></>;
-};
+  const getMovieDetails = async () => {
+    await movieDB
+      .get<MovieDetailsResponse>(`/${idMovie}`)
+      .then(res => {
+        console.log(res.data.overview);
+      })
+      .catch(err => console.log(err.error || err.message));
+  };
 
-export default useMovieDetails;
+  useEffect(() => {
+    getMovieDetails();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  return { movieDetails, getMovieDetails };
+};
