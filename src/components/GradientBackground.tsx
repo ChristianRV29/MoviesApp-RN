f996ad5323d43096546e9feed0f2078854914723
ChristianRV29 <kmylorv@gmail.com>
2022-01-26 17:15:46 -0500

    Lineargradient dependency implement

--- src/components/GradientBackground.tsx
+++ src/components/GradientBackground.tsx
@@ -1,18 +1,29 @@
 import React from 'react';
 import { StyleSheet, View } from 'react-native';
 
+import Lineargradient from 'react-native-linear-gradient';
+
 interface Props {
   children: JSX.Element | JSX.Element[];
 }
 
 const GradientBackground = ({ children }: Props) => {
-  return <View style={styles.mainContainer}>{children}</View>;
+  return (
+    <View style={styles.mainContainer}>
+      <Lineargradient
+        colors={['#084F6A', '#75CEDB', 'white']}
+        style={{ ...StyleSheet.absoluteFillObject }}
+        start={{ x: 0.1, y: 0.1 }}
+        end={{ x: 0.5, y: 0.7 }}
+      />
+      {children}
+    </View>
+  );
 };
 
 const styles = StyleSheet.create({
   mainContainer: {
     flex: 1,
-    backgroundColor: '#084F6A',
   },
 });
 
